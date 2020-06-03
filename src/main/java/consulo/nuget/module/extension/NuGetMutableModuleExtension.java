1c0c5aaf1feadbc28a150c9c736cd0d2fd40e350
VISTALL <vistall.valeriy@gmail.com>
2020-06-03 19:26:30 +0300

    compilation

--- src/main/java/consulo/nuget/module/extension/NuGetMutableModuleExtension.java
+++ src/main/java/consulo/nuget/module/extension/NuGetMutableModuleExtension.java
@@ -24,14 +24,20 @@ import com.intellij.openapi.util.Comparing;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vfs.StandardFileSystems;
 import com.intellij.openapi.vfs.VirtualFileManager;
+import consulo.disposer.Disposable;
 import consulo.module.extension.MutableModuleExtension;
+import consulo.module.extension.swing.SwingMutableModuleExtension;
 import consulo.roots.ModuleRootLayer;
+import consulo.ui.Component;
+import consulo.ui.Label;
+import consulo.ui.annotation.RequiredUIAccess;
+import consulo.ui.layout.VerticalLayout;
 
 /**
  * @author VISTALL
  * @since 24.11.14
  */
-public class NuGetMutableModuleExtension extends NuGetModuleExtension implements MutableModuleExtension<NuGetModuleExtension>
+public class NuGetMutableModuleExtension extends NuGetModuleExtension implements MutableModuleExtension<NuGetModuleExtension>, SwingMutableModuleExtension
 {
 	public NuGetMutableModuleExtension(@Nonnull String id, @Nonnull ModuleRootLayer moduleRootLayer)
 	{
@@ -50,11 +56,20 @@ public class NuGetMutableModuleExtension extends NuGetModuleExtension implements
 		}
 	}
 
+	@RequiredUIAccess
 	@Nullable
 	@Override
-	public JComponent createConfigurablePanel(@Nonnull Runnable runnable)
+	public JComponent createConfigurablePanel(@Nonnull Disposable disposable, @Nonnull Runnable runnable)
 	{
 		return new NuGetConfigPanel(this);
+	}
+
+	@RequiredUIAccess
+	@Nullable
+	@Override
+	public Component createConfigurationComponent(@Nonnull Disposable disposable, @Nonnull Runnable runnable)
+	{
+		return VerticalLayout.create().add(Label.create("Unsupported platform"));
 	}
 
 	@Override
