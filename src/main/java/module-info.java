180b98a1db45f4ab2205cb5a602fcb2a58918836
Valery Semenchuk <vistall.valeriy@gmail.com>
2026-04-17 16:22:55 +0300

    get grid from ide deps

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,32 @@
 module consulo.nuget {
     requires consulo.ide.api;
 
+    requires consulo.language.editor.api;
+    requires consulo.external.service.api;
+    requires consulo.repository.ui.api;
+    requires consulo.http.api;
+    requires consulo.ui.ex.awt.api;
+
+    requires consulo.application.api;
+    requires consulo.application.content.api;
+    requires consulo.component.api;
+    requires consulo.disposer.api;
+    requires consulo.file.chooser.api;
+    requires consulo.file.editor.api;
+    requires consulo.module.api;
+    requires consulo.module.content.api;
+    requires consulo.project.api;
+    requires consulo.project.ui.api;
+    requires consulo.ui.api;
+    requires consulo.logging.api;
+    requires consulo.localize.api;
+    requires consulo.virtual.file.system.api;
+    requires consulo.util.collection;
+    requires consulo.util.concurrent;
+    requires consulo.util.io;
+    requires consulo.util.jdom;
+    requires consulo.util.lang;
+
     requires com.intellij.xml.api;
     requires com.intellij.xml.dom.api;
     requires consulo.dotnet.api;
